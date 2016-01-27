c825efa43a0da19ba85221871764b9eef2282a85
balexander2 <awol358@gmail.com>
2016-01-27 10:36:44 +0300

    added SimpleTyps.class

---
+++ 02/helloworld.ru/SimpleTypes.java
@@ -0,0 +1,20 @@
+class SimpleTypes {
+	public static void main (String args[]) {
+		byte b = 0x55;
+		short s = 0x55ff;
+		int i = 1000000;
+		long l = 0xfffffffL;
+		char c = 'a';
+		float f = .25f;
+		double d = .00001234;
+		boolean bool = true;
+		System.out.println("byte b = "+b);
+		System.out.println("short s = "+s);
+		System.out.println("int i = "+i);
+		System.out.println("long l = "+l);
+		System.out.println("char = "+c);
+		System.out.println("float f = "+f);
+		System.out.println("double d = "+d);
+		System.out.println("boolean bool = "+bool);
+	}
+}
