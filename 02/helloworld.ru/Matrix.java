42160a08e0efc05096f8b3953a516936103ef903
balexander2 <awol358@gmail.com>
2016-01-27 11:21:07 +0300

    added matrix example

---
+++ 02/helloworld.ru/Matrix.java
@@ -0,0 +1,14 @@
+class Matrix {
+	public static void main(String args[]) {
+	double m[][];
+	m= new double[4][4];
+	m[0][0]=1;
+	m[1][1]=1;
+	m[2][2]=1;
+	m[3][3]=1;
+	System.out.println(m[0][0]+" "+m[0][1]+" "+m[0][2]+" "+m[0][3]);
+	System.out.println(m[1][0]+" "+m[1][1]+" "+m[1][2]+" "+m[1][3]);
+	System.out.println(m[2][0]+" "+m[2][1]+" "+m[2][2]+" "+m[2][3]);
+	System.out.println(m[3][0]+" "+m[3][1]+" "+m[3][2]+" "+m[3][3]);
+	}
+}
