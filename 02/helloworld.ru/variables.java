ceef5e92fee8d5034b8abcf173bcb79f05edd866
balexander2 <awol358@gmail.com>
2016-01-27 10:07:38 +0300

    added variables example

---
+++ 02/helloworld.ru/variables.java
@@ -0,0 +1,10 @@
+class Variables {
+	public static void main (String args[]) {
+		double a = 3;
+		double b = 4;
+		double c;
+		c = Math.sqrt (a*a+b*b);
+		System.out.println ("c = "+c);		
+		
+	}
+}
