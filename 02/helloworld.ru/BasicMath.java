06047ddee8163a7135155420c91b7a0fa0479749
balexander2 <awol358@gmail.com>
2016-01-27 12:49:52 +0300

    added basicmath example

---
+++ 02/helloworld.ru/BasicMath.java
@@ -0,0 +1,14 @@
+class basicMath {
+	public static void main (String args[]) {
+		int a = 1+1;
+		int b = a*3;
+		int c = b/4;
+		int d = b-a;
+		int e = -d;
+		System.out.println("a = "+a);
+		System.out.println("b = "+b);
+		System.out.println("c = "+c);
+		System.out.println("d = "+d);
+		System.out.println("e = "+e);
+	}	  
+}
