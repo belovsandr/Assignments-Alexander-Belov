4016f9beff792af00c6b6b995a549f63639a3bac
balexander2 <awol358@gmail.com>
2016-01-29 10:22:07 +0300

    classes: constructor

--- 02/helloworld.ru/Point.java
+++ 02/helloworld.ru/Point.java
@@ -1,17 +1,13 @@
 class Point {
 	int x,y;
-	void init(int x,int y) {
+	Point(int x,int y) {
 		this.x=x;
 		this.y=y;
 	}
 }
-class TwoPointsInit {
-	public static void main (String args[]) {
-		Point p1 = new Point();
-		Point p2 = new Point();
-		p1.init(10,20);
-		p2.init(42,99);
-		System.out.println("x = "+p1.x+" y = "+p1.y);
-		System.out.println("x = "+p2.x+" y = "+p2.y);
+class PointCreate {
+	public static void main(String args[]) {
+		Point p = new Point (10,20);
+		System.out.println("x = "+p.x+" y = "+p.y);	
 	}
 }
