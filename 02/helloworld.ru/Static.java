233f82363f3d39bd553c84e3a0d342270a9eac9a
balexander2 <awol358@gmail.com>
2016-01-29 11:54:48 +0300

    added classes:static demo

---
+++ 02/helloworld.ru/Static.java
@@ -0,0 +1,16 @@
+class Static {
+	static int a = 3;
+	static int b;
+	static void method (int x) {
+		System.out.println("x = "+x);
+		System.out.println("a = "+a);
+		System.out.println("b = "+b);	
+	}
+	static {
+		System.out.println("static block initialized");
+		b = a * 4;	
+	}
+	public static void main (String args[]) {
+		method (42);
+	}
+}
