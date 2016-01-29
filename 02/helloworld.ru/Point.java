5eddd7d6e4df008301d114f1b50fabb5f6362480
balexander2 <awol358@gmail.com>
2016-01-29 10:47:40 +0300

    classes: this in constructors

--- 02/helloworld.ru/Point.java
+++ 02/helloworld.ru/Point.java
@@ -4,14 +4,22 @@ class Point {
 		this.x=x;
 		this.y=y;
 	}
-	Point () {
-		x = -1;
-		y = -1;
+	double distance(int x, int y) {
+		int dx = this.x - x;
+		int dy = this.y - y;
+		return Math.sqrt(dx*dx + dy*dy);	
+	}
+	double distance(Point p) {
+		return distance(p.x, p.y);	
 	}
 }
-class PointCreateAlt {
+class PointDist {
 	public static void main(String args[]) {
-		Point p = new Point ();
-		System.out.println("x = "+p.x+" y = "+p.y);	
+		Point p1 = new Point (0,0);
+		Point p2 = new Point (30,40);
+		System.out.println("p1 = "+p1.x+", "+p1.y);
+		System.out.println("p2 = "+p2.x+", "+p2.y);
+		System.out.println("p1.distance(p2) = "+p1.distance(p2));
+		System.out.println("p1.distance(60,80) = "+p1.distance(60,80));	
 	}
 }
