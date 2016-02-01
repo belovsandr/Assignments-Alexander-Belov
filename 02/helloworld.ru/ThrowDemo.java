7eed39f04c5e751b7da20367d3c304d0c1936529
balexander2 <awol358@gmail.com>
2016-02-01 11:34:02 +0300

    added throw demo

---
+++ 02/helloworld.ru/ThrowDemo.java
@@ -0,0 +1,19 @@
+class ThrowDemo {
+	static void demoproc() {
+		try {
+			throw new NullPointerException("demo");		
+		}
+		catch (NullPointerException e) {
+			System.out.println("caught inside demoproc");
+			throw e;		
+		}	
+	}
+	public static void main (String args[]) {
+		try {
+			demoproc();		
+		}
+		catch (NullPointerException e) {
+			System.out.println("recaught: "+e);		
+		}	
+	}
+}
