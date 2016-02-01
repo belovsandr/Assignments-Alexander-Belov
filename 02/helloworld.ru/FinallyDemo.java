7d316f713e774675a4d41861593fed25bc8c65b3
balexander2 <awol358@gmail.com>
2016-02-01 11:50:09 +0300

    added finally demo

---
+++ 02/helloworld.ru/FinallyDemo.java
@@ -0,0 +1,27 @@
+class FinallyDemo {
+	static void procA() {
+		try {
+			System.out.println("inside procA");
+			throw new RuntimeException ("demo");
+		}
+		finally {
+			System.out.println("procA's finally");
+		}	
+	}
+	static void procB() {
+		try {
+			System.out.println("inside procB");
+			return;		
+		}
+		finally {
+			System.out.println("procB's finally");		
+		}	
+	}
+	public static void main(String args[]) {
+		try {
+			procA();		
+		}
+		catch (Exception e) {}
+		procB();	
+	}
+}
