9267660e1da7cc0eac0ee36eceb7f781b9deb9b9
balexander2 <awol358@gmail.com>
2016-02-04 11:39:54 +0300

    add dockerjava:hello.java

---
+++ 02/dockerjava/src/main/java/Hello.java
@@ -0,0 +1,11 @@
+package sparkexample;
+import static spark.Spark.get;
+
+public class Hello {
+	public static void main(String[] args) {
+		get("/", (req,res) -> {
+
+			return "hello from sparkjava.com";		
+		});
+	}
+}
