58d15eef8c8970f6c9e7e7d12cbc01ae0e4f558a
balexander2 <awol358@gmail.com>
2016-02-08 13:26:21 +0300

    05 random numbers

--- 03/UsefulLinks/ruby/05.rb
+++ 03/UsefulLinks/ruby/05.rb
@@ -1,7 +1,11 @@
-puts 5**2
-puts 5**0.5
-puts 7/3
-puts 7%3
-puts 365%7
-puts ((5-2).abs)
-puts ((2-5).abs)
+puts rand
+puts rand
+puts rand
+puts (rand(100))
+puts (rand(100))
+puts (rand(100))
+puts (rand(1))
+puts (rand(1))
+puts (rand(999999999999))
+puts ('The weatherman said there is a '+rand(101).to_s+'% chance of rain,')
+puts ('but you can never trust a weatherman.')
