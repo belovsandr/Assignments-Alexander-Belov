fa0b23365e554b9346b8ffd6fc8cea624ee913bc
balexander2 <awol358@gmail.com>
2016-02-08 12:31:06 +0300

    04 a few things to try:bigger and better number

--- 03/UsefulLinks/ruby/04.rb
+++ 03/UsefulLinks/ruby/04.rb
@@ -1,8 +1,3 @@
-puts 'Hello there, what\'s your first name?'
-firstname = gets.chomp
-puts 'And what is your middle name?'
-middlename = gets.chomp
-puts 'What\'s your last name?'
-lastname = gets.chomp
-fullname = firstname+' '+middlename+' '+lastname
-puts 'Your full name is '+fullname+'. Is that right?'
+puts 'What is your favourite number?'
+fav = gets.chomp
+puts 'Really? I think that '+(fav.to_i+1).to_s+' is a bigger and better number'
