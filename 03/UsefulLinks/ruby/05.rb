1edfd5cd75eeabe3ea0cdd5b9a4b3ab76d3bd287
balexander2 <awol358@gmail.com>
2016-02-08 13:20:13 +0300

    05 a few things to try:table of contents

--- 03/UsefulLinks/ruby/05.rb
+++ 03/UsefulLinks/ruby/05.rb
@@ -1,3 +1,7 @@
-puts "WHAT DO YOU WANT???????????"
-answer = gets.chomp
-puts "WHADDAYA MEAN \""+answer.upcase+"\"???!? YOU'RE FIRED!!"
+linewidth=50
+puts 'Table of Contents'.center(linewidth)
+puts ''
+puts 'Chapter 1: Numbers'.ljust(linewidth/2)+'page 1'.rjust(linewidth/2)
+puts 'Chapter 1: Letters'.ljust(linewidth/2)+'page 72'.rjust(linewidth/2)
+puts 'Chapter 1: Variables'.ljust(linewidth/2)+'page 118'.rjust(linewidth/2)
+
