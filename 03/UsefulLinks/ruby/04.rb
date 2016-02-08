efd881524443512e3c6db25fc3055e0db1f3f328
balexander2 <awol358@gmail.com>
2016-02-08 12:15:03 +0300

    04 mixing it up:another look at puts

--- 03/UsefulLinks/ruby/04.rb
+++ 03/UsefulLinks/ruby/04.rb
@@ -1,10 +1,3 @@
-puts '15'.to_f
-puts '99.999'.to_f
-puts '99.999'.to_i
-puts ''
-puts '5 is my favourite number'.to_i
-puts 'who asked about 4 or whatever?'.to_i
-puts 'your momma did'.to_f
-puts ''
-puts 'stringy'.to_s
-puts 3.to_i
+puts 20
+puts 20.to_s
+puts '20'
