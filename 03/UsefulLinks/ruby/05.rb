2909d4ee9c97661378a615175ed4d7b12fc7d7b9
balexander2 <awol358@gmail.com>
2016-02-08 13:02:45 +0300

    05 fancy string methods: length

--- 03/UsefulLinks/ruby/05.rb
+++ 03/UsefulLinks/ruby/05.rb
@@ -1,9 +1,3 @@
-var1 = 'stop'
-var2 = 'stressed'
-var3 = 'Can you pronounce this sentence backwards?'
-puts var1.reverse
-puts var2.reverse
-puts var3.reverse
-puts var1
-puts var2
-puts var3
+puts 'what is your full name?'
+name = gets.chomp
+puts 'Did you know there are '+name.length.to_s+' characters in your name, '+name+'?'
