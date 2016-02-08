300300573e780d9a654a0d205fa4cf3381c0e183
balexander2 <awol358@gmail.com>
2016-02-08 13:33:17 +0300

    05 the math object

--- 03/UsefulLinks/ruby/05.rb
+++ 03/UsefulLinks/ruby/05.rb
@@ -1,11 +1,6 @@
-puts rand
-puts rand
-puts rand
-puts (rand(100))
-puts (rand(100))
-puts (rand(100))
-puts (rand(1))
-puts (rand(1))
-puts (rand(999999999999))
-puts ('The weatherman said there is a '+rand(101).to_s+'% chance of rain,')
-puts ('but you can never trust a weatherman.')
+puts (Math::PI)
+puts (Math::E)
+puts (Math.cos(Math::PI/3))
+puts (Math.tan(Math::PI/4))
+puts (Math.log(Math::E**2))
+puts ((1+Math.sqrt(5))/2)
