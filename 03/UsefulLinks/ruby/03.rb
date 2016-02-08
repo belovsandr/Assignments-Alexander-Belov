b9376b3d57e40ca1c9646d2db54a17906f84d021
balexander2 <awol358@gmail.com>
2016-02-08 11:55:59 +0300

    03 variables and assignment name

--- 03/UsefulLinks/ruby/03.rb
+++ 03/UsefulLinks/ruby/03.rb
@@ -1,3 +1,3 @@
-myString = '...you can say that again...'
-puts myString
-puts myString
+name = 'Patricia Rosanna Jessica Mildred Oppenheimer'
+puts 'My name is ' + name+'.'
+puts 'Wow! '+name+' is a really long name!'
