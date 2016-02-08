a8fc755b5e968d042aa32218e2b776664dddd6e5
balexander2 <awol358@gmail.com>
2016-02-08 13:09:15 +0300

    05 fancy string methods: center

--- 03/UsefulLinks/ruby/05.rb
+++ 03/UsefulLinks/ruby/05.rb
@@ -1,6 +1,7 @@
-letters = 'aAbBcCdDeE'
-puts letters.upcase
-puts letters.downcase
-puts letters.swapcase
-puts letters.capitalize
-puts ' a'.capitalize
+lineWidth = 50
+puts('Old Mother Hubbard'.center(lineWidth))
+puts('Sat in her cupboard'.center(lineWidth))
+puts('Eating her curds an whey,'.center(lineWidth))
+puts('When along came a spider'.center(lineWidth))
+puts('Which sat down beside her'.center(lineWidth))
+puts('And scared her poor shoe dog away.'.center(lineWidth))
