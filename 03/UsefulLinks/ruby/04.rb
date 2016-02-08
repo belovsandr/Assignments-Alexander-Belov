36d6dfcfdd0d48e7240063cfa0ecc1819c3048a3
balexander2 <awol358@gmail.com>
2016-02-08 12:13:26 +0300

    04 mixing it up:conversions extra

--- 03/UsefulLinks/ruby/04.rb
+++ 03/UsefulLinks/ruby/04.rb
@@ -1,4 +1,10 @@
-var1 =2
-var2='5'
-puts var1.to_s+var2
-puts var1+var2.to_i
+puts '15'.to_f
+puts '99.999'.to_f
+puts '99.999'.to_i
+puts ''
+puts '5 is my favourite number'.to_i
+puts 'who asked about 4 or whatever?'.to_i
+puts 'your momma did'.to_f
+puts ''
+puts 'stringy'.to_s
+puts 3.to_i
