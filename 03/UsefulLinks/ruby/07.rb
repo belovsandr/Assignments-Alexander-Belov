b535ceba9bff39d40cc32b1b0075d20262fcf056
balexander2 <awol358@gmail.com>
2016-02-09 12:02:30 +0300

    07: More array methods

--- 03/UsefulLinks/ruby/07.rb
+++ 03/UsefulLinks/ruby/07.rb
@@ -1,3 +1,13 @@
-3.times do 
-	puts 'Hip-Hip-Hooray!'
+foods = ['artichoke','brioche','caramel']
+
+puts foods
+puts
+puts foods.to_s
+puts
+puts foods.join(', ')
+puts
+puts foods.join(' :) ')+' 8)'
+
+200.times do
+	puts []
 end
