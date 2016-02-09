20bd2e3dd5b30b51a24b23545aeba9ab590a410d
balexander2 <awol358@gmail.com>
2016-02-09 11:26:25 +0300

    06 A few things to try: Granny extended

--- 03/UsefulLinks/ruby/06.rb
+++ 03/UsefulLinks/ruby/06.rb
@@ -1,11 +1,17 @@
 ans = ''
-while ans != 'BYE'
+bye = 0
+while bye < 3
 	year = rand (1930..1950)
 	puts '(*say something to your granny, please*):'
 	ans = gets.chomp
-	if ans == ans.upcase
-		puts 'NO, NOT SINCE '+year.to_s+'!'
+	if ans == 'BYE'
+		bye = bye + 1
 	else
-		puts 'HUH?! SPEAK UP, SONNY!'
+		bye = 0
+		if ans == ans.upcase
+			puts 'NO, NOT SINCE '+year.to_s+'!'
+		else
+			puts 'HUH?! SPEAK UP, SONNY!'
+		end
 	end
 end
