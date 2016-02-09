7824491d377478581e9b91f0afa4e3e9c37e8986
balexander2 <awol358@gmail.com>
2016-02-09 12:51:02 +0300

    07: A few things to try: table of contents

--- 03/UsefulLinks/ruby/07.rb
+++ 03/UsefulLinks/ruby/07.rb
@@ -1,9 +1,8 @@
-arr = []
-word = nil
-while word != ''
-	puts 'Write your word:'
-	word = gets.chomp
-	arr.push word
+linewidth=50
+contents = [['Chapter 1: Numbers','page 1'],['Chapter 2: Letters','page 72'],['Chapter 3: Variables','page 118']]
+puts 'Table of Contents'.center(linewidth)
+puts ''
+contents.each do |(lre,rre)|
+	puts lre.ljust(linewidth/2)+rre.rjust(linewidth/2)	
 end
-puts arr.sort
 
