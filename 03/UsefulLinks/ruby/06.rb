59e68087c7f0e3621ffbe8eec7c70dd160eba482
balexander2 <awol358@gmail.com>
2016-02-09 11:04:41 +0300

    06 Looping

--- 03/UsefulLinks/ruby/06.rb
+++ 03/UsefulLinks/ruby/06.rb
@@ -1,15 +1,8 @@
-puts 'Hello and welcome to 7th grade English.'
-puts 'My name is Mrs. Gabbard. And your name is...?'
-name = gets.chomp
-if name == name.capitalize
-	puts 'Please take a seat, '+name+'.'
-else
-	puts name+ '? You mean '+ name.capitalize + ', right?'
-	puts 'Don\'t you evenn know how to spell your name??'
-	reply = gets.chomp
-	if reply.downcase == 'yes'
-		puts 'Hmmph! Well, sit down!'
-	else
-		puts 'GET OUT!!'
-	end
+command = ''
+
+while command != 'bye'
+	puts command
+	command = gets.chomp
 end
+
+puts 'Come again soon!'
