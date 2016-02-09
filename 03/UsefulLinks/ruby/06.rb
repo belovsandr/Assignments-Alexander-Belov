4bca5030141d8fca0b861c7676a5fe7ae3345a7a
balexander2 <awol358@gmail.com>
2016-02-09 10:58:58 +0300

    06 Branching: more branches

--- 03/UsefulLinks/ruby/06.rb
+++ 03/UsefulLinks/ruby/06.rb
@@ -1,8 +1,15 @@
-puts 'I\'m a fortune-teller. Tell me your name'
+puts 'Hello and welcome to 7th grade English.'
+puts 'My name is Mrs. Gabbard. And your name is...?'
 name = gets.chomp
-if name == 'Alexander'
-	puts 'I see great things in your future!'
+if name == name.capitalize
+	puts 'Please take a seat, '+name+'.'
 else
-	puts 'Your future is....Oh my! Look a the time!'
-	puts 'I really need to hurry up, sorry!'
+	puts name+ '? You mean '+ name.capitalize + ', right?'
+	puts 'Don\'t you evenn know how to spell your name??'
+	reply = gets.chomp
+	if reply.downcase == 'yes'
+		puts 'Hmmph! Well, sit down!'
+	else
+		puts 'GET OUT!!'
+	end
 end
