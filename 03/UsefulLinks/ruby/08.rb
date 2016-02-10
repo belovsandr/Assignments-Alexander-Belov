9f533ba7c132bf1019c0bc9b3734d0473bd7ea73
balexander2 <awol358@gmail.com>
2016-02-10 08:26:10 +0300

    08 local variables:little pest

--- 03/UsefulLinks/ruby/08.rb
+++ 03/UsefulLinks/ruby/08.rb
@@ -1,5 +1,8 @@
-def doubleThis num
-  numTimes2=num*2
-  puts num.to_s+' doubled is '+numTimes2.to_s
+def littlePest var
+  var = nil
+  puts 'HAHA! I ruined your variable'
 end
-doubleThis 44
+
+var = 'You can\'t even touch my variable!'
+littlePest var
+puts var 
