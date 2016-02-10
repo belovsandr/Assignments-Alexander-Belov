25b9d7ea5a316cfee8cb0798ffbb1de3f89ac0c7
balexander2 <awol358@gmail.com>
2016-02-10 08:45:57 +0300

    08 return values psychology test

--- 03/UsefulLinks/ruby/08.rb
+++ 03/UsefulLinks/ruby/08.rb
@@ -1,7 +1,31 @@
-def sayMoo numberOfMoos
-  puts 'moooooo....'*numberOfMoos
-  'yellow submarine'
+def ask question
+  goodAnswer = false
+  while (not goodAnswer)
+    puts question
+    reply = gets.chomp.downcase
+    if (reply == 'yes' or reply == 'no')
+      goodAnswer = true
+      if reply == 'yes'
+        answer = true
+      else
+        answer = false
+      end
+    else
+      puts 'Please answer "yes" or "no"!'
+    end   
+  end
+  answer
 end
 
-x=sayMoo 2
-puts x
+ask 'Do you like eating tacos?'
+ask 'Do you like eating burritos?'
+wetsBed = ask 'Do you wet the bed?'
+ask 'Do you like eating chimchangas?'
+ask 'Do you like eating sopapillas?'
+ask 'Do you like eating tamales?'
+ask 'Do you like drinking horchata?'
+ask 'Do you like eating flautas?'
+puts
+puts "Thank you"
+puts
+puts wetsBed
