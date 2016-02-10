1ee7f1b7420e5172d67e53aa0a11503c1f5d756c
balexander2 <awol358@gmail.com>
2016-02-10 11:34:45 +0300

    ruby the hard way exercise 05

---
+++ 03/UsefulLinks/rubythehardway/ex05.rb
@@ -0,0 +1,17 @@
+name = 'Alexander B.'
+age = 22
+height = 180
+weight = 70
+eyes = 'Blue'
+teeth = 'White'
+hair='Black'
+
+puts "Let's talk about #{name}."
+puts "He's #{height} cm tall."
+puts "He's #{weight} kg heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth}."
+
+#this line is tricky 
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}"
