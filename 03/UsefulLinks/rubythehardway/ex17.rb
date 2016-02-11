5802b1e0df365da7ac5a8f118509f5fa0a94a909
balexander2 <awol358@gmail.com>
2016-02-11 09:03:49 +0300

    ruby the hard way exercise 17

---
+++ 03/UsefulLinks/rubythehardway/ex17.rb
@@ -0,0 +1,19 @@
+from_file, to_file = ARGV
+
+puts "Copying from #{from_file} to #{to_file}"
+
+indata = open(from_file).read
+
+puts "The input file is #{indata.length} bytes long"
+
+puts "Does the output file exist? #{File.exist?(to_file)}"
+puts "Ready, hit RETURN to continue, CTRL-C to abort"
+$stdin.gets
+
+out_file = open(to_file,'w')
+out_file.write(indata)
+
+puts "Alright, all done."
+
+out_file.close
+
